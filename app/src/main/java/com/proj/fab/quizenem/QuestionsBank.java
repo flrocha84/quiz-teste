5ec1ab6477d30d0a096df3423cb0a20ca653e98e
Fabio Rocha <fabiolr@gmail.com>
2022-03-13 14:36:27 -0300

    aleatorias e com explicacao

--- app/src/main/java/com/proj/fab/quizenem/QuestionsBank.java
+++ app/src/main/java/com/proj/fab/quizenem/QuestionsBank.java
@@ -11,13 +11,12 @@ public class QuestionsBank {
     private static List<QuestionsList> quimicaQuestions(){
         final List<QuestionsList> questionsLists = new ArrayList<>();
         //criando as perguntas
-
-        final QuestionsList question1 = new QuestionsList("1 - Quem matou o rei?", "o palhaço", "o cão", "o gato","o assassino","o assassino","","por causa");
-        final QuestionsList question2 = new QuestionsList("2 - Que cor é azul?", "azul","preto", "branco","amarelo", "azul","","por causa");
-        final QuestionsList question3 = new QuestionsList("3 - Que cor é preta?","azul","preto", "branco","amarelo", "preto","","por causa");
-        final QuestionsList question4 = new QuestionsList("4 - Que cor é branco?","azul","preto", "branco","amarelo", "branco","","por causa");
-        final QuestionsList question5 = new QuestionsList("Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","por causa");
-        final QuestionsList question6 = new QuestionsList("Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","por causa");
+        final QuestionsList question1 = new QuestionsList("Q1 - Quem matou o rei?", "o palhaço", "o cão", "o gato","o assassino","o assassino","","porque o assassino é ruim");
+        final QuestionsList question2 = new QuestionsList("Q2 - Que cor é azul?", "azul","preto", "branco","amarelo", "azul","","porque azul é a cor do céu");
+        final QuestionsList question3 = new QuestionsList("Q3 - Que cor é preta?","azul","preto", "branco","amarelo", "preto","","porque é escuro");
+        final QuestionsList question4 = new QuestionsList("Q4 - Que cor é branco?","azul","preto", "branco","amarelo", "branco","","poque é a cor da paz");
+        final QuestionsList question5 = new QuestionsList("Q5 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","porque parece o sol");
+        final QuestionsList question6 = new QuestionsList("Q6 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","já falei que parece com o sol");
 
         // adicionando na lista
         questionsLists.add(question1);
@@ -26,6 +25,7 @@ public class QuestionsBank {
         questionsLists.add(question4);
         questionsLists.add(question5);
         questionsLists.add(question6);
+        Collections.shuffle(questionsLists);
         return questionsLists;
     }
 
@@ -33,13 +33,12 @@ public class QuestionsBank {
     private static List<QuestionsList> fisicaQuestions(){
         final List<QuestionsList> questionsLists = new ArrayList<>();
         //criando as perguntas
-
-        final QuestionsList question1 = new QuestionsList("F1 - Quem matou o rei?", "o palhaço", "o cão", "o gato","o assassino","o assassino","","por causa");
-        final QuestionsList question2 = new QuestionsList("F2 - Que cor é azul?", "azul","preto", "branco","amarelo", "azul","","por causa");
-        final QuestionsList question3 = new QuestionsList("F3 - Que cor é preta?","azul","preto", "branco","amarelo", "preto","","por causa");
-        final QuestionsList question4 = new QuestionsList("F4 - Que cor é branco?","azul","preto", "branco","amarelo", "branco","","por causa");
-        final QuestionsList question5 = new QuestionsList("F5 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","por causa");
-        final QuestionsList question6 = new QuestionsList("F6 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","por causa");
+        final QuestionsList question1 = new QuestionsList("F1 - Quem matou o rei?", "o palhaço", "o cão", "o gato","o assassino","o assassino","","porque o assassino é ruim");
+        final QuestionsList question2 = new QuestionsList("F2 - Que cor é azul?", "azul","preto", "branco","amarelo", "azul","","porque azul é a cor do céu");
+        final QuestionsList question3 = new QuestionsList("F3 - Que cor é preta?","azul","preto", "branco","amarelo", "preto","","porque é escuro");
+        final QuestionsList question4 = new QuestionsList("F4 - Que cor é branco?","azul","preto", "branco","amarelo", "branco","","poque é a cor da paz");
+        final QuestionsList question5 = new QuestionsList("F5 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","porque parece o sol");
+        final QuestionsList question6 = new QuestionsList("F6 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","já falei que parece com o sol");
 
         // adicionando na lista
         questionsLists.add(question1);
@@ -56,12 +55,12 @@ public class QuestionsBank {
         final List<QuestionsList> questionsLists = new ArrayList<>();
         //criando as perguntas
 
-        final QuestionsList question1 = new QuestionsList("1 - Quem matou o rei?", "o palhaço", "o cão", "o gato","o assassino","o assassino","","por causa");
-        final QuestionsList question2 = new QuestionsList("2 - Que cor é azul?", "azul","preto", "branco","amarelo", "azul","","por causa");
-        final QuestionsList question3 = new QuestionsList("3 - Que cor é preta?","azul","preto", "branco","amarelo", "preto","","por causa");
-        final QuestionsList question4 = new QuestionsList("4 - Que cor é branco?","azul","preto", "branco","amarelo", "branco","","por causa");
-        final QuestionsList question5 = new QuestionsList("Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","por causa");
-        final QuestionsList question6 = new QuestionsList("Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","por causa");
+        final QuestionsList question1 = new QuestionsList("B1 - Quem matou o rei?", "o palhaço", "o cão", "o gato","o assassino","o assassino","","porque o assassino é ruim");
+        final QuestionsList question2 = new QuestionsList("B2 - Que cor é azul?", "azul","preto", "branco","amarelo", "azul","","porque azul é a cor do céu");
+        final QuestionsList question3 = new QuestionsList("B3 - Que cor é preta?","azul","preto", "branco","amarelo", "preto","","porque é escuro");
+        final QuestionsList question4 = new QuestionsList("B4 - Que cor é branco?","azul","preto", "branco","amarelo", "branco","","poque é a cor da paz");
+        final QuestionsList question5 = new QuestionsList("B5 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","porque parece o sol");
+        final QuestionsList question6 = new QuestionsList("B6 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","já falei que parece com o sol");
 
         // adicionando na lista
         questionsLists.add(question1);
@@ -77,12 +76,12 @@ public class QuestionsBank {
         final List<QuestionsList> questionsLists = new ArrayList<>();
         //criando as perguntas
 
-        final QuestionsList question1 = new QuestionsList("1 - Quem matou o rei?", "o palhaço", "o cão", "o gato","o assassino","o assassino","","porque o assassino é ruim");
-        final QuestionsList question2 = new QuestionsList("2 - Que cor é azul?", "azul","preto", "branco","amarelo", "azul","","porque azul é a cor do céu");
-        final QuestionsList question3 = new QuestionsList("3 - Que cor é preta?","azul","preto", "branco","amarelo", "preto","","porque é escuro");
-        final QuestionsList question4 = new QuestionsList("4 - Que cor é branco?","azul","preto", "branco","amarelo", "branco","","poque é a cor da paz");
-        final QuestionsList question5 = new QuestionsList("5 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","porque parece o sol");
-        final QuestionsList question6 = new QuestionsList("6 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","já falei que parece com o sol");
+        final QuestionsList question1 = new QuestionsList("H1 - Quem matou o rei?", "o palhaço", "o cão", "o gato","o assassino","o assassino","","porque o assassino é ruim");
+        final QuestionsList question2 = new QuestionsList("H2 - Que cor é azul?", "azul","preto", "branco","amarelo", "azul","","porque azul é a cor do céu");
+        final QuestionsList question3 = new QuestionsList("H3 - Que cor é preta?","azul","preto", "branco","amarelo", "preto","","porque é escuro");
+        final QuestionsList question4 = new QuestionsList("H4 - Que cor é branco?","azul","preto", "branco","amarelo", "branco","","poque é a cor da paz");
+        final QuestionsList question5 = new QuestionsList("H5 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","porque parece o sol");
+        final QuestionsList question6 = new QuestionsList("H6 - Que cor é amarelo?","azul","preto", "branco","amarelo", "amarelo","","já falei que parece com o sol");
 
         // adicionando na lista
 
@@ -106,7 +105,7 @@ public static List<QuestionsList> getQuestions(String selectTopicName){
             default:
                 return historiaQuestions();
         }
-}
+  }
 
 
 }
